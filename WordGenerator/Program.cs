a602c8d65fd79881f9fb090663572c9c0af18a62
Olfredos6 <Alfred.nehemie@gmail.com>
2015-08-10 01:17:22 +0200

    First commit

---
+++ WordGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Create("Dico.xt");
+
+            StreamReader reader;
+            StreamWriter writer = new StreamWriter("Dico.txt");
+
+            writer.WriteLine("Fuuuuuck");
+
+            
+        }
+    }
+}
